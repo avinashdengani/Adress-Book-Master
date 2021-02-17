2f123e22ff674b24d9d4ad3c28329202ee873bc7
Avinash Dengani <avinashdengani123@gmail.com>
2021-02-17 17:36:42 +0530

    Completed index and helper functions

---
+++ includes/functions.inc.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Provides function to basic operations that will be performed frequently
+*/
+
+/**
+ * @return connection or false
+ */
+function db_connect()
+{
+    static $connection;
+    if(!isset($connection))
+    {
+        $config = parse_ini_file('config.ini');
+        $connection = mysqli_connect($config['host'], $config['username'] ,$config['password'], $config['dbname']);
+    }
+    return $connection;
+}
+function db_query($query)
+{
+    $connection = db_connect();
+    if($connection)
+    {
+        $result = mysqli_query($connection, $query);
+        return $result;
+    }
+    return false;
+}
+function db_error()
+{
+    $connection = db_connect();
+    return mysqli_error($connection);
+}
+
+function db_select($query)
+{
+    $rows = array();
+    $result = db_query($query);
+    if($result === false)
+    {
+        return false;
+    }
+
+    while($row = mysqli_fetch_assoc($result))
+    {
+        $rows[] = $row;
+    }
+    return $rows;
+}
